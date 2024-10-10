ab7208f1ad5ecf0d17f0f3da094fea4aaf1ed576
GUILHERME PARDELHAS DE ARAUJO <202402697668@alunos.ibmec.edu.br>
2024-10-10 16:56:43 -0300

    atualizando

---
+++ src/components/Cartao/Cartao.jsx
@@ -0,0 +1,13 @@
+import { Container, Texto, Titulo } from "./Style";
+
+const Cartao = ( props ) => (
+    <Container>
+        <Titulo>
+            {props.titulo} 
+        </Titulo>
+        <Texto> 
+            {props.texto} 
+        </Texto>
+    </Container>
+);
+export default Cartao;
---
+++ src/pages/SobreNos.jsx
@@ -0,0 +1,32 @@
+import Cartao from "../components/Cartao/Cartao";
+import ListContainer from "../components/ListContainer/ListContainer";
+import Base from "./Base";
+
+const dados = [
+    {
+        titulo:"Um titulo de Array.",
+        texto:"um texto grande do Array."
+    },
+    {
+        titulo:"Um outro titulo do Array.",
+        texto:"Um outro texto do Array."
+    }
+]
+
+const SobreNos = () => (
+    <Base>
+       <ListContainer>
+       {
+           dados.map( (ele, i) => (
+                   <Cartao
+                       key={i}
+                       titulo={ele.titulo}
+                       texto={ele.texto}
+                    />
+           ))
+       }
+       </ListContainer>
+    </Base>
+);
+
+export default SobreNos;
